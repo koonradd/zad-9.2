958438d1192732c33a354433e862222e4675d4f1
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-01-11 22:23:07 +0100

    Add

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat (maleNames);
+var newName = 'Marian'; 
+if (allNames.indexOf(newName) === -1) {
+    console.log(allNames);
+    allNames.push('Marian');
+    console.log(allNames);
+}
